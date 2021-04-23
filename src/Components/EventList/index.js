d8b2e4513f296cdaa4585ec31203f0cfb85aaebe
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-04-23 11:27:51 +0100

    event list

--- src/Components/EventList/index.js
+++ src/Components/EventList/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import data from "../../data";
 
 function EventList() {
   return (
     <div>
-      <h2>Event List</h2>
+      <ul>
+        {data.map((events) => (
+          <div>
+            <li>{events.Name}</li>
+            <li>{events.Description}</li>
+            <li>{events.Date}</li>
+            <li>{events.Time}</li>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+var data = [
+  {
+    id: 1,
+    Name: "Litter Picking",
+    Description: "At the park",
+    Date: "23/4/21",
+    Time: "10:00",
+    Latitude: "",
+    Longitude: "",
+  },
+  {
+    id: 2,
+    Name: "Litter Picking",
+    Description: "At the park",
+    Date: "23/4/21",
+    Time: "10:00",
+    Latitude: "",
+    Longitude: "",
+  },
+];
+
+export default data;
